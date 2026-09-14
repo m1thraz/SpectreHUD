01c1bd6348ae034314da6405955c94aaa2cfb137
m1thraz <boehser-.-onkel@hotmail.de>
2026-09-14 11:52:32 +0200

    ci fixes 2

--- ui/controllers/loot_controller.py
+++ ui/controllers/loot_controller.py
@@ -484,7 +484,7 @@ class LootController(QObject):
             return []
 
         rendered_cards: List[QWidget] = []
-        for category in sorted(CATEGORIES, key=lambda c: c.get("order", 0)):
+        for category in sorted(CATEGORIES, key=lambda c: int(c.get("order", 0))):
             cat_entries = [e for e in loot_entries if e.get("category") == category["id"]]
             if not cat_entries:
                 continue
